950533a275b694ece7c65d8d464bd574a8d97ec8
Arthur Rocha <arthurrochafts@gmail.com>
2021-12-04 12:45:26 -0300

    start project

---
+++ api/Models/EntityModel/TransactionInstallmentModels/TransactionInstallment.cs
@@ -0,0 +1,14 @@
+﻿namespace api.Models.EntityModel.TransactionInstallmentModels
+{
+    public class TransactionInstallment
+    {
+        public long ID { get; set; }
+        public long TransactionID { get; set; }
+        public long ParcelNumber { get; set; }
+        public decimal GrossAmount { get; set; }
+        public decimal NetAmount { get; set; }
+        public decimal? AnticipatedAmount { get; set; }
+        public DateTime ExpectedReceiptDate { get; set; }
+        public DateTime TransferDate { get; set; }
+    }
+}
